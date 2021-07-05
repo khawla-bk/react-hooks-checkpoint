366b80d89b9f8f511e5bd7afbb7180a8f5d39a55
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-05 07:44:00 -0400

    Update List.js

--- reacthooks_checkpoint/src/components/List.js
+++ reacthooks_checkpoint/src/components/List.js
@@ -1,11 +1,17 @@
 import MovieCard from "./Card";
 
-function List({ movies }) {
+function List({ movies, searchValue, rate }) {
   return (
     <div>
-      {movies.map((el, i) => (
-        <MovieCard movies={el} />
-      ))}
+      {movies
+        .filter(
+          (el) =>
+            el.name.toUpperCase().includes(searchValue.toUpperCase()) &&
+            el.rate >= rate
+        )
+        .map((el, i) => (
+          <MovieCard movies={el} />
+        ))}
     </div>
   );
 }
