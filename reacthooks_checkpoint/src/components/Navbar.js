b5e13a63eaf879cc4199b65e1d7ea8648bd5b95b
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-05 07:42:31 -0400

    Create Navbar.js

---
+++ reacthooks_checkpoint/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+function Navbar() {
+  return (
+    <Nav defaultActiveKey="/home" as="ul">
+      <strong>
+        {" "}
+        <Nav.Item as="li">
+          <Nav.Link href="/home">Homepage</Nav.Link>
+        </Nav.Item>{" "}
+      </strong>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="link-1">Details</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="link-2">More Links</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
