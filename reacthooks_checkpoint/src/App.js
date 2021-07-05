8dd3f129adbedc956bedfa56a6f50500488595e4
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-05 07:45:21 -0400

    Update App.js

--- reacthooks_checkpoint/src/App.js
+++ reacthooks_checkpoint/src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import React from "react";
 import "./App.css";
-import StarRating from './components/StarRating';
+import StarRating from "./components/StarRating";
 import Search from "./components/Search";
 import List from "./components/List";
 import AddMovie from "./components/AddMovie";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [movies, setMovies] = useState([
@@ -13,8 +14,8 @@ function App() {
         "https://m.media-amazon.com/images/M/MV5BMTg4ODkzMDQ3Nl5BMl5BanBnXkFtZTgwNTEwMTkxMDE@._V1_.jpg",
       name: "Mr. Nobody",
       year: "2009",
-      star: 5,
-      duration: "157 minutes",
+      rate: 5,
+
       description:
         "The film tells the life story of Nemo Nobody, a 118-year-old man who is the last mortal on Earth after the human race has achieved quasi-immortality.",
       id: 1
@@ -24,8 +25,8 @@ function App() {
         "https://images-na.ssl-images-amazon.com/images/I/914I-VXn3cL._RI_.jpg",
       name: "I Origin",
       year: "2014",
-      star: 4,
-      duration: "106 minutes",
+      rate: 4,
+
       description:
         "I Origins is an independent drama sci-fi , written, produced and directed by Mike Cahill, released in 2014.",
       id: 2
@@ -35,8 +36,8 @@ function App() {
         "https://i.pinimg.com/originals/11/1c/5c/111c5c9ad99661af2d80e38948cf29d8.jpg",
       name: "Interstellar",
       year: "2014",
-      star: 3,
-      duration: "169 minutes",
+      rate: 3,
+
       description:
         "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
       id: 3
@@ -46,8 +47,8 @@ function App() {
         "https://pics.filmaffinity.com/The_House_at_the_End_of_Time-114727023-large.jpg",
       name: "The House At The End Of Time",
       year: "2013",
-      star: 4,
-      duration: "101 minutes",
+      rate: 4,
+
       description:
         "Dulce encounters apparitions in her house and unleashes a terrible prophecy. Thirty years later, Dulce, now an old woman, returns to unravel the mystery that has terrorized her for years.",
       id: 4
@@ -57,8 +58,8 @@ function App() {
         "https://images-na.ssl-images-amazon.com/images/I/51TfUWh5N9L._SY445_.jpg",
       name: "Inception",
       year: "2010",
-      star: 4,
-      duration: "148 minutes",
+      rate: 4,
+
       description:
         "Inception is a thriller of science fiction American-British written, directed and produced by Christopher Nolan, released in 2010.",
       id: 5
@@ -68,8 +69,8 @@ function App() {
         "https://m.media-amazon.com/images/M/MV5BZjZlZDlkYTktMmU1My00ZDBiLWFlNjEtYTBhNjVhOTM4ZjJjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
       name: "Donnie Darko",
       year: "2001",
-      star: 4,
-      duration: "108 minutes",
+      rate: 4,
+
       description:
         "Donnie Darko, an awkward teenager, befriends Frank, a figure in a bunny costume only he can see, who informs Donnie that the world will end in 28 days, 6 hours, 42 minutes, and 12 seconds.",
       id: 6
@@ -78,47 +79,31 @@ function App() {
       img: "https://movieposters2.com/images/635972-b.jpg",
       name: "Sherlock Holmes",
       year: "2009",
-      star: 5,
-      duration: "129 minutes",
+      rate: 5,
+
       description:
         "Detective Sherlock Holmes and his partner, Dr Watson, send Blackwood, a serial killer, to the gallows. However, they are shocked to learn that he is back from the dead and must pursue him again.",
       id: 7
     }
   ]);
-  title: "";
-    rate: 1
 
-  Add = (newMovie) => {
-    this.setState({
-      movies: [...this.state.movies, { ...newMovie, id: Date.now() }]
-    });
-  };
-  search = (movies) => {
-    this.setState({
-      movies: movies
-    });
-  };
-
-  Rate = (rate) => {
-    this.setState({
-      rate: rate
-    });
-  };
+  const [searchValue, setsearchValue] = useState("");
+  const [rate, setRate] = useState(1);
 
   return (
     <div>
-      <h1> Recommended Movies</h1>
-
-      <div className="App">
-        <Search search={this.search} />
-        <AddMovie Add={this.Add} />
+      <div className="nav">
+        <Navbar />
+        <Search setsearchValue={setsearchValue} />
       </div>
-      <StarRating Rate={this.Rate} />
 
-      <List
-      
-        movies={movies}
-      />
+      <div className="filter">
+        <p> Filter By Rating: </p>
+        <StarRating setRate={setRate} /> <br />
+        <br />
+      </div>
+      <List movies={movies} searchValue={searchValue} rate={rate} />
+      <AddMovie Add={AddMovie} />
     </div>
   );
 }
